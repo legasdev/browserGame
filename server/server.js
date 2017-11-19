153029c18df422c221bbc85b5822bdd1e36aa602
Artem Stepanov <jekke-x@yandex.ru>
2017-11-19 18:01:12 +0300

    Update 19.11.17

--- server/server.js
+++ server/server.js
@@ -4,38 +4,6 @@ var mongoClient = require("mongodb").MongoClient;
 var objectId = require("mongodb").ObjectID;
 var md5 = require("./md5");
 
-// подключенные клиенты
-var clients = {};
-var players = {};
-var rooms = {}; //игровые комнаты
-
-players[0] = {};
-players[1] = {};
-//типо игрок с бд
-players[1].name = "legasdev";
-players[1].id = 1;
-
-players[0].name = "wikend";
-players[0].id = 2;
-
-rooms[0] = {};
-rooms[1] = {};
-rooms[2] = {};
-//типо комната создана
-rooms[1].idr = "1";
-rooms[1].maxPlayers = 1;
-rooms[1].players = {};
-rooms[1].players[1] = players[1];
-rooms[1].isPlay = true;
-rooms[1].whoPlay = 1;
-
-rooms[2].idr = 2;
-rooms[2].maxPlayers = 1;
-rooms[2].players = {};
-rooms[2].players[1] = players[0];
-rooms[2].isplay = false;
-rooms[2].whoPlay = 1;
-
 //* Смотрим БД *//
 var app = express();
 var jsonParser = bodyParser.json();
@@ -84,7 +52,8 @@ app.post("/api/users", jsonParser, function (req, res) {
 	nowData /= 1000;
     var userLogin = req.body.login;
     var userPass = req.body.pass;
-    var user = {login: userLogin, pass: userPass, sh: md5(userLogin+nowData.toString())};
+	var userSh = req.body.sh;
+    var user = {login: userLogin, pass: userPass, sh: md5(md5(userLogin)+md5(nowData.toString()))};
 	
     mongoClient.connect(url, function(err, db){
 		if (type == 0) {
@@ -137,7 +106,34 @@ app.post("/api/users", jsonParser, function (req, res) {
 				}
 			});
 		}
-    });
+		//создание комнаты
+		else if (type == 2) {
+			var _players = {};
+			var _room;
+			//добавляем с комнату того, кто создал ее
+			console.log(userSh);
+			db.collection("users").findOne({sh: userSh} , function(err, _user){
+				_players[1] = {};
+				_players[1] = _user;
+
+				//создаем комнату с начальными параметрами
+				_room = {idr: md5(new Date()),
+						maxPlayers: 1,
+						players: _players,
+						isPlay: true,
+						whoPlay: 1
+						};
+
+				//добавляем комнату в БД
+				db.collection("rooms").insertOne(_room, function(err, result) {
+					if(err) return res.status(400).send();
+				})
+				console.log(_room);
+				res.send(_room.idr); //ответ в виде объекта комнаты (потом только idr)
+				db.close(); //закрываем коннект
+			});
+		}
+	});
 });
   
 app.delete("/api/users/:id", function(req, res){
@@ -193,16 +189,14 @@ function rand(min, max)
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/*
+==========
+			ИГРОВОЙ СЕРВЕР
+==========
+*/
+
 //веб сервер
 var WebSocketServer = new require('ws');
-
-//игровая карта
-var map = {};
-map.l = 0;
-map.r = 1000;
-map.t = 0;
-map.b = 1000;
-var startMass = 30;
 
 // WebSocket-сервер на порту 443
 var webSocketServer = new WebSocketServer.Server({
@@ -210,15 +204,7 @@ var webSocketServer = new WebSocketServer.Server({
 });
 
 webSocketServer.on('connection', function(ws) {
-  	//var id = Math.random(); //присваиваем id
-  	//clients[id] = ws;
-	//заносим данные об игроке и ставим его в 0, 0
-	//players[id] = {};
-	//players[id].x = rand(map.l, map.r);
-	//players[id].y = rand(map.t, map.b);
-	//players[id].s = startMass;
-	
-	ws.send(JSON.stringify({'type':'map', 'data':map}));
+	//ws.send(JSON.stringify({'type':'map', 'data':map}));
 	
 	/*//посылаем данные клиентам
 	var interval = setInterval(function() {
@@ -240,19 +226,25 @@ webSocketServer.on('connection', function(ws) {
 
 	//закрываем моединение
 	ws.on('close', function() {
-		delete clients[id];
-		delete players[id];
-		clearInterval(interval);
+		//clearInterval(interval);
 	});
 	
 	/* 
 		Функция ответчик комнаты и соединения
 	*/
-	function echos(idr) {
+	function echos(_idr) {
 		//отправляем данные (комнату)
-		if (ws && ws.readyState == 1) {
-			ws.send(JSON.stringify({'type':'idr', 'data':rooms[idr]}));
-		}
+		//Коннектимся с БД
+		mongoClient.connect(url, function(err, db){
+			//отправляем данные по полученному idr
+			if (ws && ws.readyState == 1) {
+				db.collection("rooms").findOne({idr: _idr}, function(err, _obj){
+					ws.send(JSON.stringify({'type':'echo', 'data':_obj.idr}));
+					db.close();
+				});
+				
+			}
+		});
 	}
 });
 
