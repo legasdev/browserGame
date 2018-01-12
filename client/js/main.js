9b18fcce15da27b2ef3abf867216bd006a0e184e
dz1rt <dm8205@yandex.ru>
2018-01-12 19:22:10 +0300

    update 12.01.18

---
+++ client/js/main.js
@@ -0,0 +1,17 @@
+function login() {
+	console.log("asd");
+	// var btn=getElementsByClassName('loginArea')[0];
+	// console.log(getElementsByClassName('loginArea'));
+	// btn.style.display='grid';
+
+}
+
+function registration() {
+	console.log("asd");
+}
+// console.log(getElementsByClassName('top'));
+function borderMain() {
+	setTimeout(function(){
+
+	},1);
+}
