35aa230127b68188592974ca28f142a0b546a42a
dz1rt <dm8205@yandex.ru>
2018-01-12 19:43:06 +0300

    update2 12.01.18

--- client/js/main.js
+++ client/js/main.js
@@ -1,13 +1,25 @@
+
+
 function login() {
 	console.log("asd");
-	// var btn=getElementsByClassName('loginArea')[0];
-	// console.log(getElementsByClassName('loginArea'));
-	// btn.style.display='grid';
+	var lg=document.getElementsByClassName('loginArea')[0];
+var btn=document.getElementsByClassName('button');
+btn[0].style.display="none";
+btn[1].style.display="none";
+lg.style.display="grid"
+
+
 
 }
 
 function registration() {
 	console.log("asd");
+	var rg=document.getElementsByClassName('regArea')[0];
+var btn=document.getElementsByClassName('button');
+btn[0].style.display="none";
+btn[1].style.display="none";
+rg.style.display="grid"
+
 }
 // console.log(getElementsByClassName('top'));
 function borderMain() {
