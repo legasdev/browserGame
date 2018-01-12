754684e17fc168c2c8a97a91ab05ff1b67df1212
dz1rt <dm8205@yandex.ru>
2018-01-12 20:00:27 +0300

    update4 12.01.18

--- client/js/main.js
+++ client/js/main.js
@@ -6,7 +6,10 @@ function login() {
 var btn=document.getElementsByClassName('button');
 btn[0].style.display="none";
 btn[1].style.display="none";
-lg.style.display="grid"
+lg.style.display="grid";
+lg.classList.add('animated','fadeIn');
+
+
 
 
 
@@ -19,7 +22,7 @@ var btn=document.getElementsByClassName('button');
 btn[0].style.display="none";
 btn[1].style.display="none";
 rg.style.display="grid"
-
+rg.classList.add('animated','fadeIn');
 }
 // console.log(getElementsByClassName('top'));
 function borderMain() {
